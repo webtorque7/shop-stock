4c089b55b17c3c5093e86bba05351d67ca92ec05
Carey <carey@webtorque.co.nz>
2016-05-25 19:24:24 +1200

    save the locale as well

--- code/OrderStockExtension.php
+++ code/OrderStockExtension.php
@@ -8,7 +8,9 @@
  */
 class OrderStockExtension extends DataExtension
 {
-    private static $db = array();
+    private static $db = array(
+        'OverwriteLocale' => 'Varchar'
+    );
 
     private static $has_one = array(
         'StoreWarehouse' => 'StoreWarehouse'
@@ -22,6 +24,11 @@ class OrderStockExtension extends DataExtension
             //not working, must get from pre request..
 //            $warehouse = StoreWarehouse::current();
 
+
+            $parts = explode('-', ShoppingCart::$cartid_session_name);
+
+            $this->owner->OverwriteLocale = isset($parts[1]) ? $parts[1] : '';
+
             $storeID = filter_var(ShoppingCart::$cartid_session_name, FILTER_SANITIZE_NUMBER_INT);
             $warehouse = ShopStore::get()->byID($storeID)->StoreWarehouse();
 
@@ -29,5 +36,9 @@ class OrderStockExtension extends DataExtension
                 $this->owner->StoreWarehouseID = $warehouse->ID;
             }
         }
+    }
+
+    public function getLocale(){
+        return $this->owner->OverwriteLocale;
     }
 }

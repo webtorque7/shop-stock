9de5c8187e45335aab7232221a74d45a842af333
Carey <carey@webtorque.co.nz>
2016-06-10 15:33:47 +1200

    second thought the relation should be as part of this module

--- code/OrderStockExtension.php
+++ code/OrderStockExtension.php
@@ -6,4 +6,9 @@
  * Date: 24/05/2016
  * Time: 12:27 PM
  */
-class OrderStockExtension extends DataExtension{}
+class OrderStockExtension extends DataExtension
+{
+    private static $has_one = array(
+        'StoreWarehouse' => 'StoreWarehouse'
+    );
+}

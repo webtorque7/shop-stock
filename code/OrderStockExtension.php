e6dc6ca8140bca09a038c0b07a1e7ed010fbc98f
Carey <carey@webtorque.co.nz>
2016-06-10 13:06:11 +1200

    Moved all these to main project because they are more likely to be specific to the project

--- code/OrderStockExtension.php
+++ code/OrderStockExtension.php
@@ -6,32 +6,4 @@
  * Date: 24/05/2016
  * Time: 12:27 PM
  */
-class OrderStockExtension extends DataExtension
-{
-    private static $db = array(
-        'Locale' => 'Varchar'
-    );
-
-    private static $has_one = array(
-        'StoreWarehouse' => 'StoreWarehouse'
-    );
-
-    public function onBeforeWrite()
-    {
-        parent::onBeforeWrite();
-        if(!$this->owner->StoreWarehouseID){
-            $storeID = filter_var(ShoppingCart::$cartid_session_name, FILTER_SANITIZE_NUMBER_INT);
-            if($storeID){
-                $warehouse = ShopStore::get()->byID($storeID)->StoreWarehouse();
-                if($warehouse && $warehouse->exists()){
-                    $this->owner->StoreWarehouseID = $warehouse->ID;
-                }
-            }
-        }
-
-        if(!$this->owner->Locale){
-            $locale = substr(ShoppingCart::$cartid_session_name, strpos(ShoppingCart::$cartid_session_name, '-') + 1);
-            $this->owner->Locale = $locale;
-        }
-    }
-}
+class OrderStockExtension extends DataExtension{}
